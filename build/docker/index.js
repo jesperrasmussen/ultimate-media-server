b9220713f4af67f99d5153aceeb5cd2da1fd90e9
Jesper Rasmussen <me@jesperrasmussen.com>
2016-05-28 18:11:02 +0200

    Removes unneeded configurations

--- build/docker/index.js
+++ build/docker/index.js
@@ -7,33 +7,6 @@ const chalk = require('chalk');
 const cfgPath = cwd('build/docker/docker-compose.json');
 const dcPath = cwd('docker-compose.yml');
 const dcObj = require(cfgPath);
-const pia = {
-  user: process.env.PIA_USER || '',
-  password: process.env.PIA_PASS || ''
-};
-const transmission = {
-  user: process.env.TRANSMISSION_USER || '',
-  password: process.env.TRANSMISSION_PASS || ''
-};
-
-if (!pia.user && !pia.password) {
-  console.log(chalk.yellow(`PIA credentials not provided. Will not build Transmission.`));
-
-  delete dcObj.transmission;
-  delete dcObj.nginx;
-
-  dcObj.sonarr.links = dcObj.sonarr.links.filter((link) => link !== 'transmission');
-  dcObj.couchpotato.links = dcObj.couchpotato.links.filter((link) => link !== 'transmission');
-  dcObj.couchpotatoprerelease.links = dcObj.couchpotatoprerelease.links.filter((link) => link !== 'transmission');
-}
-else if (transmission.user && transmission.password) {
-  dcObj.transmission.environment.push('TRANSMISSION_RPC_USERNAME=${TRANSMISSION_USER}');
-  dcObj.transmission.environment.push('TRANSMISSION_RPC_PASSWORD=${TRANSMISSION_PASS}');
-  dcObj.transmission.environment.push('TRANSMISSION_RPC_AUTHENTICATION_REQUIRED=true');
-}
-else {
-  console.log(chalk.yellow(`Transmission credentials not provided. Transmission auth disabled.`));
-}
 
 console.log('Converting to yaml...');
 const yamlStr = y2j.stringify(dcObj);
